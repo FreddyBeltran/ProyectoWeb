126ae80ff2eca00610d3b7335d38d84a5a4da9fe
shukishan <A00825235@itesm.mx>
2022-11-21 21:21:58 -0600

    Update post.js

--- back/src/api/post.js
+++ back/src/api/post.js
@@ -1,35 +1,6 @@
 
 const db = require('../db/connect')
 
-const register = async (req, res) => {
-    const { username, password, firstnames, lastnames, email } = req.body;
-
-    let [email_check] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-    if (email_check.length > 0) {
-        return res.status(400).send('Duplicated email.')
-    }
-    let [username_check] = await db.query('SELECT * FROM users WHERE username = ?', [username]);
-    if (username_check.length > 0) {
-        return res.status(400).send('Duplicated username.')
-    }
-    if(password == ""){
-        return res.status(400).send('Invalid password.')
-    }
-    let user = await db.query('INSERT INTO users SET ?', 
-    { 
-        username: username, 
-        password: password,
-        firstnames: firstnames,
-        lastnames: lastnames,
-        email: email,
-        updated_at: "2022-01-01 00:00:00"
-    });
-    if(user) {
-        return res.status(200).send("Succesful.");
-    } 
-    return res.status(400);
-}
-
 const login = async (req, res) => {
     const { username, password } = req.body;
     let [user] = await db.query('SELECT * FROM users WHERE username = ?', username);
@@ -93,24 +64,4 @@ const changeUser = async (req, res) => {
     }
 }
 
-const addFriend = async (req, res) => {
-    const { username } = req.body;
-    let [user] = await db.query('SELECT * FROM users WHERE username = ?', username);
-    if(!user) {
-        return res.status(400);
-    }
-    
-    
-}
-
-const deleteFriend = async (req, res) => {
-    const { username } = req.body;
-    let [user] = await db.query('SELECT * FROM users WHERE username = ?', username);
-    if(!user) {
-        return res.status(400);
-    }
-    
-}
-
-module.exports = { register, login, changeUser, changePassword, 
-    addFriend, deleteFriend } // , addRating, changeRating, addComment, changeComment, addMovie, deleteMovie, createList, deleteList, setStatus, changeTitle, changeDescription 
+module.exports = { login, changeUser, changePassword } // , addRating, changeRating, addComment, changeComment, addMovie, deleteMovie, createList, deleteList, setStatus, changeTitle, changeDescription 
